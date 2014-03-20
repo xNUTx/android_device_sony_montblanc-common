d2abcfe7257735ef35866ba5efe177fc4c75a8cf
percy-g2 <gahlotpercy@gmail.com>
2014-03-21 05:13:53 +0530

    fix mismatch path

--- montblanc.mk
+++ montblanc.mk
@@ -107,7 +107,7 @@ PRODUCT_COPY_FILES += \
     device/sony/montblanc-common/config/omxloaders:system/etc/omxloaders \
     device/sony/montblanc-common/config/ril_config:system/etc/ril_config \
     device/sony/montblanc-common/config/install_wlan.sh:system/bin/install_wlan.sh \
-    device/sony/montblanc-common/config/ste_modem.sh:system/etc/ste_modem.sh
+    device/sony/montblanc-common/config/ste_modem.sh:system/etc/ste_modem.sh \
     device/sony/montblanc-common/config/gps.conf:system/etc/gps.conf\
    
    
