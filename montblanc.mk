8a67f55855bf08bba80f8fcd01c62222ca086347
95A31 <x95a31xreg@gmail.com>
2014-03-28 17:42:49 +0100

    File Manager + Torch

--- montblanc.mk
+++ montblanc.mk
@@ -60,18 +60,17 @@ PRODUCT_PACKAGES += \
         hostapd_cli \
         hostapd
 
+
 # Lights HAL library
-PRODUCT_PACKAGES += \
-        lights.montblanc
+PRODUCT_PACKAGES += lights.montblanc
+
 
 # b2r2lib
-PRODUCT_PACKAGES += \
-        libblt_hw
+PRODUCT_PACKAGES += libblt_hw
 
  
 # libomxil-bellagio
-PRODUCT_PACKAGES += \
-        libomxil-bellagio
+PRODUCT_PACKAGES += libomxil-bellagio
     
     
 # libtinyalsa & audio.usb.default
@@ -83,13 +82,11 @@ PRODUCT_PACKAGES += \
 
        
 # WiFi config utility
-PRODUCT_PACKAGES += \
-       iw
+PRODUCT_PACKAGES += iw
 
        
 # libaudioparameter
-PRODUCT_PACKAGES += \
-       libaudioparameter
+PRODUCT_PACKAGES += libaudioparameter
 
 # Hostapd
 PRODUCT_PACKAGES += \
@@ -98,25 +95,32 @@ PRODUCT_PACKAGES += \
 
        
 # Dbus
-PRODUCT_PACKAGES += \
-       libdbus
+PRODUCT_PACKAGES += libdbus
 
        
 # lib_net_iface_cmd
-PRODUCT_PACKAGES += \
-       libnetcmdiface
+PRODUCT_PACKAGES += libnetcmdiface
        
 
 # Libasound
-PRODUCT_PACKAGES += \
-       libasound
+PRODUCT_PACKAGES += libasound
+
 
 #FM Radio
 # We must adapt Qualcomm FM Radio app
 
+
+# File Manager
+PRODUCT_PACKAGES += CMFileManager
+
+
+# Torch
+PRODUCT_PACKAGES += Torch
+
+
 # Misc
-PRODUCT_PACKAGES += \
-       com.android.future.usb.accessory
+PRODUCT_PACKAGES += com.android.future.usb.accessory
+
 
 # Custom init scripts
 PRODUCT_COPY_FILES += \
@@ -126,10 +130,12 @@ PRODUCT_COPY_FILES += \
         device/sony/montblanc-common/config/init.st-ericsson.rc:root/init.st-ericsson.rc \
 	device/sony/montblanc-common/config/ueventd.st-ericsson.rc:root/ueventd.st-ericsson.rc  
 
+
 # Post recovery script
 PRODUCT_COPY_FILES += \
 	device/sony/montblanc-common/recovery/postrecoveryboot.sh:root/sbin/postrecoveryboot.sh \
 	device/sony/montblanc-common/config/modelid_cfg.sh:system/bin/modelid_cfg.sh
+
 
 # Hardware configuration scripts
 PRODUCT_COPY_FILES += \
