55f0039c5dfc4e9723d6f28e74706996cab6d410
adeel <alwaysadeel@gmail.com>
2014-04-15 12:24:03 +0500

    Revert "fix build"
    This reverts commit ec27dee04d8402259d77d56003bdd3e35904100b.

--- montblanc.mk
+++ montblanc.mk
@@ -9,7 +9,7 @@ $(call inherit-product-if-exists, vendor/google/gapps.mk)
 $(call inherit-product-if-exists, vendor/sony/montblanc-common/montblanc-common-vendor.mk)
 
 # Common montblanc headers
-#TARGET_SPECIFIC_HEADER_PATH := device/sony/montblanc-common/include
+TARGET_SPECIFIC_HEADER_PATH := device/sony/montblanc-common/include
 
 # Common montblanc settings overlays
 DEVICE_PACKAGE_OVERLAYS += device/sony/montblanc-common/overlay
