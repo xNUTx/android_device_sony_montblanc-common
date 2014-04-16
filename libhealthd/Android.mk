07c2bdd50914ce21d84a1443a57642b45958ea08
percy-g2 <gahlotpercy@gmail.com>
2014-04-16 18:42:29 +0530

    Fix healthd out dir

--- libhealthd/Android.mk
+++ libhealthd/Android.mk
@@ -17,5 +17,6 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := healthd_board_montblanc.cpp
 LOCAL_MODULE := libhealthd.montblanc
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_C_INCLUDES := system/core/healthd
 include $(BUILD_STATIC_LIBRARY)
