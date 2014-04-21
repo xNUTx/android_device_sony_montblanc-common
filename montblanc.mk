3624e7374c41ff69cc3878a9d85672e8cc03c87a
DevSwift <devswift@wp.pl>
2014-04-21 16:08:03 +0200

    A-GPS fix

--- montblanc.mk
+++ montblanc.mk
@@ -128,7 +128,8 @@ PRODUCT_COPY_FILES += \
         device/sony/montblanc-common/config/ril_config:system/etc/ril_config \
         device/sony/montblanc-common/config/install_wlan.sh:system/bin/install_wlan.sh \
         device/sony/montblanc-common/config/ste_modem.sh:system/etc/ste_modem.sh \
-        device/sony/montblanc-common/config/gps.conf:system/etc/gps.conf
+        device/sony/montblanc-common/config/gps.conf:system/etc/gps.conf \
+		device/sony/montblanc-common/config/cacert.txt:system/etc/suplcert/cacert.txt
 
 # Garbage Collector type
 PRODUCT_TAGS += dalvik.gc.type-precise
