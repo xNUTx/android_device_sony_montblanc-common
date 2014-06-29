a179bb3e7346898eee8553f25ba25bf64466ba37
Abhinav Jhanwar <abhinav.jhanwar.august2@gmail.com>
2014-06-29 19:34:12 +0530

    Update montblanc.mk

--- montblanc.mk
+++ montblanc.mk
@@ -97,8 +97,7 @@ PRODUCT_COPY_FILES += \
         device/sony/montblanc-common/config/init.rc:root/init.rc \
         device/sony/montblanc-common/config/init.environ.rc:root/init.environ.rc \
         device/sony/montblanc-common/config/init.st-ericsson.rc:root/init.st-ericsson.rc \
-        device/sony/montblanc-common/config/ueventd.st-ericsson.rc:root/ueventd.st-ericsson.rc \
-        external/koush/Superuser/init.superuser.rc:root/init.superuser.rc
+        device/sony/montblanc-common/config/ueventd.st-ericsson.rc:root/ueventd.st-ericsson.rc
 
 # Post recovery script
 PRODUCT_COPY_FILES += \
