53d4981d3c14c316a32e0213195b9a1f8dc5fabb
Alberto García <garcia98ftw@gmail.com>
2014-11-21 07:17:07 +0100

    Fix build

--- custombootimg.mk
+++ custombootimg.mk
@@ -19,7 +19,6 @@ $(INSTALLED_BOOTIMAGE_TARGET): $(PRODUCT_OUT)/kernel $(recovery_ramdisk) $(INSTA
 	$(hide) cp $(BOOTREC_LED) $(PRODUCT_OUT)/combinedroot/sbin/
 
 	$(hide) cp -R $(PRODUCT_OUT)/recovery/root/sbin/* $(PRODUCT_OUT)/root/sbin/
-	$(hide) cp $(LOCAL_PATH)/config/init.environ.rc $(PRODUCT_OUT)/root/
 
 	$(hide) $(MKBOOTFS) $(PRODUCT_OUT)/root > $(PRODUCT_OUT)/ramdisk.cpio
 	$(hide) cp $(PRODUCT_OUT)/ramdisk.cpio $(PRODUCT_OUT)/combinedroot/sbin/
